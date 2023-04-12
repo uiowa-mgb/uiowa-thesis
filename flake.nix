8a1dd7fcea221f449064caa5c40e1cd611054978
Ethan Rooke <ethan@roo.ke>
2023-04-11 19:58:10 -0500

    build: add nix based dev shell

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Devshell for uiowa-thesis";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    # This technically pulls in more stuff than necessary to build
+    tex = pkgs.texlive.combine {
+      inherit
+        (pkgs.texlive)
+        scheme-basic # A good minimal base for latex
+        biber        # Our bibiolgraphy backend
+        biblatex     # Generating the bibliography more better
+        lipsum       # Generates random text
+        memoir       # The base of our class
+        mwe          # Lets us use example images
+        xetex        # The actual tex engine to use
+        hypdoc       # Used to build the ctan documentation
+        ;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        tex
+        pkgs.scons
+        pkgs.glow          # make help text prettier
+        pkgs.inotify-tools # watches for changes
+      ];
+
+      shellHook = ''
+        clear
+        (
+          echo "Welcome to a dev-shell!"
+          echo "Run \`make help\` to see all available targets"
+        ) | glow
+      '';
+    };
+  };
+}
